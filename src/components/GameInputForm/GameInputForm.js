3cad05eda8786c230b5150fd9febdb0a00f471a0
Manuele Sapia <manuele.sapia.comprend@outlook.com>
2024-02-22 16:01:54 +0000

    GuessInput done

--- src/components/GameInputForm/GameInputForm.js
+++ src/components/GameInputForm/GameInputForm.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 function GameInputForm() {
+
+const [textInput, setTextInput] = React.useState("");
+
   return <div>
     <form class="guess-input-wrapper">
       <label for="guess-input">Enter guess:</label>
-      <input id="guess-input" type="text" />
+      <input id="guess-input" type="text" minLength="5" value={textInput} onChange={(event) => {
+        setTextInput(event.target.value.toUpperCase());
+        console.log(event.target.value.toUpperCase());
+        }} 
+      />
     </form>
   </div>;
 }
